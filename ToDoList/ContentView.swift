cf3bc561dc0a6dc6430245a1134857fd40a48b5a
LilyBurton <lily.burton1992@gmail.com>
2023-10-06 10:39:32 +0100

    update toggles

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var todoTask = true
-    @State private var todoTextFields: [String] = ["Todo 1", "Todo 2", "Todo 3", "Todo 4", "Todo 5"]
+    @State private var todoTextFields: [String]
+    
+    @State private var todoTaskStates: [Bool]
+        
+        init() {
+            _todoTextFields = State(initialValue: ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"])
+            _todoTaskStates = State(initialValue: Array(repeating: false, count: 5))
+        }
     
     var body: some View {
         VStack {
@@ -19,19 +25,23 @@ struct ContentView: View {
                     .resizable(resizingMode: .stretch)
                     .aspectRatio(contentMode: .fit)
                     .imageScale(.medium)
+                Text("TodoList         ")
+                    .font(.largeTitle)
+                + Text("Toggle them all")
                 Section(header: Text("Todo Checklist")) {
                     ForEach(0..<todoTextFields.count, id: \.self) { index in
                         HStack {
                             TextField(todoTextFields[index], text: $todoTextFields[index])
-                            Toggle("", isOn: $todoTask)
+                            Toggle("", isOn: $todoTaskStates[index])
                         }
                     }
+
                     
-                    if todoTask {
+                    if todoTaskStates.allSatisfy { $0 == true } {
                         Text("Good Job!")
                     }
                 }
-                .padding()
+                .padding(5)
             }
         }
     }
